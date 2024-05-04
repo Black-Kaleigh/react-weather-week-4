05ea38e4e4a85ccb088cfe4a833fc89b49e00b11
Black-Kaleigh <BLACKTAMARAKALEIGH@GMAIL.COM>
2024-05-04 17:21:55 +0200

    Added the footer

---
+++ src/Footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      This project was coded by
+      <a href="https://github.com/black-kaleigh" target="_blank">
+        Black Kaleigh
+      </a>
+      and is
+      <a
+        href="https://github.com/Black-Kaleigh/Kaleigh-Vanilla-weather-app"
+        target="_blank"
+      >
+        open sourced on GitHub
+      </a>
+      and hosted
+      <a
+        href="https://app.netlify.com/sites/kaleigh-vanilla-weather-app/deploys"
+        target="_blank"
+      >
+        on Netlify
+      </a>
+    </footer>
+  );
+}
