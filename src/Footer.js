bca50136d0997b638e4e7730c353d2a7b4f8218a
Black-Kaleigh <BLACKTAMARAKALEIGH@GMAIL.COM>
2024-05-08 10:21:44 +0200

    Added rel to links

--- src/Footer.js
+++ src/Footer.js
@@ -4,13 +4,18 @@ export default function Footer() {
   return (
     <footer>
       This project was coded by
-      <a href="https://github.com/black-kaleigh" target="_blank">
+      <a
+        href="https://github.com/black-kaleigh"
+        target="_blank"
+        rel="noreferrer noopener"
+      >
         Black Kaleigh
       </a>
       and is
       <a
         href="https://github.com/Black-Kaleigh/Kaleigh-Vanilla-weather-app"
         target="_blank"
+        rel="noreferrer noopener"
       >
         open sourced on GitHub
       </a>
@@ -18,6 +23,7 @@ export default function Footer() {
       <a
         href="https://app.netlify.com/sites/kaleigh-vanilla-weather-app/deploys"
         target="_blank"
+        rel="noreferrer noopener"
       >
         on Netlify
       </a>
